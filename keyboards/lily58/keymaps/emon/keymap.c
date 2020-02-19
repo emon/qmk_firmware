e77f290c3e5537af7585824e65ec4dca84bb3fc9
KOMURA Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2020-02-19 10:48:55 +0900

    RAISE, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, KC_RGUI, KC_BSPC,LOWER

--- keyboards/lily58/keymaps/emon/keymap.c
+++ keyboards/lily58/keymaps/emon/keymap.c
@@ -198,7 +198,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   TapDLsftCaps,KC_Z, KC_X,   KC_C,    KC_V,    KC_B, KC_LBRC, TapDEqlRbrc, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS, \
   /*                  KC_LALT,KC_LGUI,TapSpcLctl, TapDLang2Raise, TapDLang1Lower, TapSpcLsft, KC_ENT, KC_BSPC \*/
-            KC_LALT,KC_LGUI,TapSpcLctl, KC_SPC, KC_ENT, KC_BSPC, TapDLang2Raise, TapDLang1Lower \
+  /*                     KC_LALT,KC_LGUI,TapSpcLctl, TapDLang2Raise, TapDLang1Lower, KC_ENT, TapSpcLsft, KC_BSPC \*/
+  RAISE,  KC_LALT, KC_LGUI, KC_SPC, KC_ENT, KC_RGUI, KC_BSPC,LOWER \
 
 ),
 /* LOWER
